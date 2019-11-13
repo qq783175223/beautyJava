6e05042b950f8a88c3698f013fcb1183ea87278b
xuzhijvn <783175223@qq.com>
2019-11-13 09:17:59 +0800

    hashcode

---
+++ hashcode-equals/src/main/java/com/xwj/HashCode.java
@@ -0,0 +1,14 @@
+package com.xwj;
+
+/**
+ * Create by xuzhijun.online on 2019/11/10.
+ */
+public class HashCode {
+
+    //hashcode返回int类型
+    public static void main(String[] args) {
+        System.out.println("xuzhijun".hashCode());
+        System.out.println("xuweijun".hashCode());
+    }
+
+}
