6ba4e63ebab52b7dfb865bd8d5e20af42709e1ea
xuzhijvn <783175223@qq.com>
2019-11-19 17:17:16 +0800

    注解

---
+++ annotation/src/main/java/com/xzj/Jiecha.java
@@ -0,0 +1,12 @@
+package com.xzj;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Create by xuzhijun.online on 2019/11/19.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Jiecha {
+
+}
