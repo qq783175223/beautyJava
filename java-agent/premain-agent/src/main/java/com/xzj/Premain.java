be9284a10fdefe76e0d338e654f167162ac4189a
xuzhijvn <783175223@qq.com>
2020-01-08 17:41:18 +0800

    JVM启动前静态Instrument

---
+++ java-agent/premain-agent/src/main/java/com/xzj/Premain.java
@@ -0,0 +1,38 @@
+/*
+ *       Copyright© (2020) ICBC Co., Ltd.
+ *
+ *       This file is part of did-http-service.
+ *
+ *       did-http-service is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Lesser General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       did-http-service is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Lesser General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Lesser General Public License
+ *       along with did-http-service.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+package com.xzj;
+
+import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
+
+/**
+ * @Author: xuzj01@sdc.icbc.com.cn
+ * @Description:
+ * @File: Premain
+ * @Version: 1.0.0
+ * @Date: 2020/1/8 10:57
+ */
+
+public class Premain {
+    public static void premain(String agentArgs, Instrumentation inst)  throws ClassNotFoundException, UnmodifiableClassException {
+        inst.addTransformer(new Transformer());
+    }
+}
---
+++ java-agent/premain-app/src/main/java/com/xzj/TestMainInJar.java
@@ -0,0 +1,35 @@
+/*
+ *       Copyright© (2020) ICBC Co., Ltd.
+ *
+ *       This file is part of did-http-service.
+ *
+ *       did-http-service is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Lesser General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       did-http-service is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Lesser General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Lesser General Public License
+ *       along with did-http-service.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+package com.xzj;
+
+/**
+ * @Author: xuzj01@sdc.icbc.com.cn
+ * @Description:
+ * @File: TestMainInJar
+ * @Version: 1.0.0
+ * @Date: 2020/1/8 10:47
+ */
+
+public class TestMainInJar {
+    public static void main(String[] args) {
+        System.out.println(new TransClass().getNumber());
+    }
+}
